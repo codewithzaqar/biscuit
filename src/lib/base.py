a43ac17987b0e24f48a4b35cc0ad3a401e1ef785
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-08 20:32:09 +0400

    feat: implement Config loader and instantiate config/bindings in Base

--- src/lib/base.py
+++ src/lib/base.py
@@ -1,11 +1,17 @@
 import tkinter as tk
 
+import lib.config as config
+
 from lib.editor import Editor
 from lib.statusbar import SLabel, SButton, StatusBar
 
 class Base:
     def __init__(self, root, *args, **kwargs):
         self.root = root
+
+        self.config = config.Config()
+        self.config.load_data()
+        self.bindings = config.Bindings()
 
         self.editor = Editor(self.root)
         self.statusbar = StatusBar(self.root)
