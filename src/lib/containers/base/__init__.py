fca40c84e8775dc0acc98f21e44384d5cdea39ca
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-09 19:16:59 +0400

    feat: build PanedWindow layout hierarchy (base/top/bottom/left/right)

--- src/lib/containers/base/__init__.py
+++ src/lib/containers/base/__init__.py
@@ -1,2 +1,15 @@
-class BasePane:
-    pass
+import tkinter as tk
+
+from lib.containers.base.top import TopPane
+from lib.containers.base.bottom import BottomPane
+
+class BasePane(tk.PanedWindow):
+    def __init__(self):
+        super().__init__()
+        self.configure(orient=tk.VERTICAL)
+
+        self.top = TopPane(self)
+        self.bottom = BottomPane(self)
+
+        self.add(self.top)
+        self.add(self.bottom)
