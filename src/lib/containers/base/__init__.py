d86312ee64ea28febde079ab403100ca6e1f93c5
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-09 19:32:39 +0400

    feat: size panes, rename to TopLeft/TopRight, add configure debug output

--- src/lib/containers/base/__init__.py
+++ src/lib/containers/base/__init__.py
@@ -3,13 +3,22 @@ import tkinter as tk
 from lib.containers.base.top import TopPane
 from lib.containers.base.bottom import BottomPane
 
+
+def topsize(event):
+    print(f" ◆ Top: {event.widget.winfo_width()}x{event.widget.winfo_height()}")
+
+def bottomsize(event):
+    print(f" ◆ Bottom: {event.widget.winfo_width()}x{event.widget.winfo_height()}")
+
 class BasePane(tk.PanedWindow):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.configure(orient=tk.VERTICAL)
 
-        self.top = TopPane(self)
-        self.bottom = BottomPane(self)
+        self.top = TopPane(self, height=520)
+        self.top.bind("<Configure>", topsize)
+        self.bottom = BottomPane(self, height=280)
+        self.bottom.bind("<Configure>", bottomsize)
 
         self.add(self.top)
         self.add(self.bottom)
--- src/testgrid.py
+++ src/testgrid.py
@@ -5,9 +5,16 @@ import lib.containers.base as containers
 from lib.editor import Editor
 from tkterminal import Terminal
 
+
+def size(event):
+    print(f"■ Root: {event.widget.winfo_width()}x{event.widget.winfo_height()}")
+
+
 root = tk.Tk()
 
-base = containers.BasePane()
+base = containers.BasePane(root, width=1290, height=800)
 base.pack(fill=tk.BOTH, expand=1)
+
+root.bind("<Configure>", size)
 
 root.mainloop()
