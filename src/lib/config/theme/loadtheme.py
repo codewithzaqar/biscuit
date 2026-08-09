230d11dbb5af69c8e1aa851a9204d5d934e14201
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-09 18:37:39 +0400

    fix: resolve ThemeLoader attribute mismatch and default theme_name

--- src/lib/config/theme/loadtheme.py
+++ src/lib/config/theme/loadtheme.py
@@ -5,18 +5,18 @@ class ThemeLoader:
     def __init__(self, theme_name, default="default"):
         self.theme_name = theme_name
         self.default = default
-        self.try_load_theme()
+        self.theme_data = self.try_load_theme()
 
     def try_load_theme(self):
         try:
-            self.theme_data = self.load_theme()
+            return self.load_theme()
         except Exception:
-            self.theme_data = self.load_theme(self.default)
+            return self.load_theme(self.default)
 
-    def load_theme(self, name=None):
-        name = name or self.theme_name
-        with open(f'src/config/themes/{name}.json', 'r') as theme_file:
-            return json.load(theme_file)
+    def load_theme(self, theme_name):
+        with open(f'src/config/themes/{theme_name}.json', 'r') as theme_file:
+           theme_data = json.load(theme_file) 
+        return theme_data
 
     def get_loaded_theme(self):
         return self.theme_data
