23d8337592723cb63c1f53e9422b8b2a09444b08
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-10 21:26:09 +0400

    chore: remove hardcoded placeholder tabs from TopRightPane

--- src/lib/containers/top/right/__init__.py
+++ src/lib/containers/top/right/__init__.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 
 from lib.components.editortabs import EditorTabs
-from lib.components.editor import Editor
 
 class TopRightPane(tk.PanedWindow):
     def __init__(self, master, *args, **kwargs):
@@ -12,6 +11,6 @@ class TopRightPane(tk.PanedWindow):
         self.editortabs.configure(height=25, width=75)
         self.add(self.editortabs)
 
-        # Add two default tabs with editors
-        self.editortabs.add(Editor(self), text="Untitled")
-        self.editortabs.add(Editor(self), text="base.py")
+        # self.editor = Editor(self)
+        # self.editor.configure(height=25, width=75)
+        # self.add(self.editor)
