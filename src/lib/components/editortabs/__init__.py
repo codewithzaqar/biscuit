e643cc1a7f453fdc5d5f11cc8d5c79a2b80e54cd
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-10 23:09:37 +0400

    refactor: track tabs by full path to support duplicate filenames

--- src/lib/components/editortabs/__init__.py
+++ src/lib/components/editortabs/__init__.py
@@ -15,9 +15,13 @@ class EditorTabs(ttk.Notebook):
         self.opened_tabs_data = {}
         self.opened_editors = {}
 
+        # data
+        # {path: name}
+
+        # editors
+        # {path: [name, editor]}
+
     def drop(self, event):
-        # Orphaned editor bug is FIXED! (test = Editor(...) is gone)
-        path = event.data.strip('{}')
         if os.path.isfile(event.data):
             self.base.add_to_open_files(file=event.data)
         elif os.path.isdir(event.data):
@@ -26,27 +30,31 @@ class EditorTabs(ttk.Notebook):
         self.base.trace(f"Dropped file: {event.data}")
 
     def update_tabs(self):
-        for i in self.base.opened_files:
-            filename = os.path.basename(i)
-            if filename not in self.opened_tabs_data.keys():
-                self.opened_tabs_data[filename] = i
-                self.base.trace(f"Tab<{filename}> was added.")
+        for filepath in self.base.opened_files:
+            filename = os.path.basename(filepath)
+            if filepath not in self.opened_tabs_data.keys():
+
+                self.opened_tabs_data[filepath] = filename
+                self.base.trace(f"Tab<{filepath}> was added.")
 
         self.update_opened_editors()
         self.base.trace(f"Opened Tabs {self.opened_tabs_data}")
 
     def update_opened_editors(self):
-        for i in self.opened_tabs_data.keys():
-            if i not in self.opened_editors:
-                self.add_editor(i)
+        for path, name in self.opened_tabs_data.items():
+            if path not in self.opened_editors.keys():
+                self.add_editor(name, path)
         self.base.trace(f"Opened editors {self.opened_editors.keys()}")
 
-    def add_editor(self, i):
-        self.opened_editors[i] = Editor(self, path=self.opened_tabs_data[i])
-        self.opened_editors[i].configure(height=25, width=75)
-        self.add(self.opened_editors[i], text=i)
+    def add_editor(self, name, path):
+
+        self.opened_editors[path] = [name, Editor(self, path=path)]
+        self.opened_editors[path][1].configure(height=25, width=75)
+        
+
+        self.add(self.opened_editors[path][1], text=name)
 
         # NEW: switch to newly added tab
-        self.select(self.opened_editors[i])
+        self.select(self.opened_editors[path][1])
 
-        self.base.trace(f"Editor<{i}> was added.")
+        self.base.trace(f"Editor<{path}> was added.")
