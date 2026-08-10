b23659165dddbc5b9d531e46bc1f1a9058006af1
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-10 22:29:31 +0400

    refactor: convert dnd.py scratch script into reusable DND utility class

--- src/lib/utils/dnd.py
+++ src/lib/utils/dnd.py
@@ -1,19 +1,15 @@
 import tkinter as tk
 from tkinter import ttk
-import tkinterDnD
 
-root = tkinterDnD.Tk()
-root.title("example")
+class DND():
+    def __init__(self, master, *args, **kwargs):
+        self.master = master
+        self.data = None
 
-stringvar = tk.StringVar()
-stringvar.set('Drop \'em here')
+        self.setup()
 
-def drop(event):
-    print(event.data)
+    def drop(event):
+        print(event.data)
 
-label_2 = ttk.Label(root, ondrop=drop,
-            textvar=stringvar, padding=100, relief="solid")
-label_2.pack(fill="both", expand=True, padx=10, pady=10)
-
-
-root.mainloop()
+    def setup(self):
+        self.master.configure(ondrop=self.drop)
