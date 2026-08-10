61afd5797b6400e22e5c21ae34271f5ba02455d3
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-10 21:49:59 +0400

    feat: load file contents into editor tabs and refactor tab tracking

--- src/lib/components/editor/__init__.py
+++ src/lib/components/editor/__init__.py
@@ -1,10 +1,13 @@
 from lib.components.text import Text
-from lib.utils.binder import Binder
+from lib.components.editor.utils.binder import Binder
 
 class Editor(Text):
-    def __init__(self, master, *args, **kwargs):
+    def __init__(self, master, path, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         self.base = master.base
+
+        self.path = path
+        self.load_file(path=self.path)
 
         self.configure(font=self.base.settings.font)
         # self.binder = Binder(bindings=self.base.settings.bindings, editor=self)
--- src/lib/components/editortabs/__init__.py
+++ src/lib/components/editortabs/__init__.py
@@ -1,5 +1,4 @@
 import os
-import tkinter as tk
 from tkinter import ttk
 
 from lib.components.editor import Editor
@@ -10,25 +9,29 @@ class EditorTabs(ttk.Notebook):
         super().__init__(master, *args, **kwargs)
         self.base = master.base
 
-        self.opened_tab_names = []
+        self.opened_tabs_data = {}
         # self.opened_tab_ids = {}
         self.opened_editors = {}
 
     def update_tabs(self):
         for i in self.base.opened_files:
             filename = os.path.basename(i)
-            if filename not in self.opened_tab_names:
-                self.opened_tab_names.append(filename)
+            if filename not in self.opened_tabs_data.keys():
+                self.opened_tabs_data[filename] = i
                 self.base.trace(f"Tab<{filename}> was added.")
 
         self.update_opened_editors()
-
-        self.base.trace(f"Opened Tabs {self.opened_tab_names}")
+        self.base.trace(f"Opened Tabs {self.opened_tabs_data}")
 
     def update_opened_editors(self):
-        for i in self.opened_tab_names:
+        for i in self.opened_tabs_data.keys():
             if i not in self.opened_editors:
-                self.opened_editors[i] = Editor(self)
-                self.opened_editors[i].configure(height=25, width=75)
-                self.add(self.opened_editors[i], text=i)
+                self.add_editor(i)
         self.base.trace(f"Opened editors {self.opened_editors.keys()}")
+
+    def add_editor(self, i):
+        self.opened_editors[i] = Editor(self, path=self.opened_tabs_data[i])
+        self.opened_editors[i].configure(height=25, width=75)
+        self.add(self.opened_editors[i], text=i)
+
+        self.base.trace(f"Editor<{i}> was added.")
