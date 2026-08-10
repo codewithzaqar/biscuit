c6cdac9f21e107aa6237d69e73e9f9bbd1249921
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-10 18:59:47 +0400

    refactor: wrap tkterminal in Terminal component; simplify BottomPane

--- src/lib/containers/bottom/__init__.py
+++ src/lib/containers/bottom/__init__.py
@@ -1,12 +1,11 @@
 import tkinter as tk
 
-from tkterminal import Terminal
+from lib.components.terminal import Terminal
 
 class BottomPane(tk.PanedWindow):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         self.base = master.base
 
-        self.t = Terminal(self, pady=5, padx=5, font=("Consolas", 15))
-        self.t.shell = True
-        self.add(self.t)
+        self.terminal = Terminal(self)
+        self.add(self.terminal)
