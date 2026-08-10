da2f281eaa3213b0a2015e0ac1efc5a66838ac44
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-10 23:43:56 +0400

    feat: integrate Tcl proxy into Text class for live <<Change>> events

---
+++ src/lib/components/text/__init__.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+
+
+class Text(tk.Text):
+    def __init__(self, master, path, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.base = master.base
+
+        self.path = path
+        self.data = None
+        self.load_file(self.path)
+
+        self.configure(font=self.base.settings.font)
+        self.create_proxy()
+
+    def create_proxy(self):
+        self._orig = self._w + "_orig"
+        self.tk.call("rename", self._w, self._orig)
+        self.tk.createcommand(self._w, self._proxy)
+
+    def load_file(self, path):
+        with open(path, 'r') as data:
+            self.data = data.read()
+            self.clear_insert()
+
+    def clear_insert(self):
+        self.clear()
+        self.write(text=self.data)
+        
+    def clear(self):
+        self.delete(1.0, tk.END)
+
+    def write(self, text):
+        self.insert(tk.END, text)
+
+    def _proxy(self, *args):
+        if args[0] == 'get' and (args[1] == tk.SEL_FIRST and args[2] == tk.SEL_LAST) and not self.tag_ranges(tk.SEL): 
+            return
+        if args[0] == 'delete' and (args[1] == tk.SEL_FIRST and args[2] == tk.SEL_LAST) and not self.tag_ranges(tk.SEL): 
+            return
+
+        cmd = (self._orig,) + args
+        result = self.tk.call(cmd)
+
+        if (args[0] in ("insert", "replace", "delete") or 
+            args[0:3] == ("mark", "set", "insert") or
+            args[0:2] == ("xview", "moveto") or
+            args[0:2] == ("xview", "scroll") or
+            args[0:2] == ("yview", "moveto") or
+            args[0:2] == ("yview", "scroll")
+        ):
+            self.event_generate("<<Change>>", when="tail")
+            
+        return result
