2e86649fccc99fbb97e3fcb804bc7a401070635f
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-15 18:35:52 +0400

    feat: add text extraction, cursor tracking, and scroll utilities to Text widget

--- src/lib/components/text/__init__.py
+++ src/lib/components/text/__init__.py
@@ -1,7 +1,4 @@
 import tkinter as tk
-import tkinter.font as Font
-
-from lib.components.text.utils import Utils
 
 
 class Text(tk.Text):
@@ -38,6 +35,43 @@ class Text(tk.Text):
 
     def write(self, text):
         self.insert(tk.END, text)
+
+    # ----- NEW: Text & Cursor Utilites -----
+
+    def get_all_text(self):
+        return self.get(1.0, tk.END)
+
+    def get_selected(self):
+        return self.get(tk.SEL_FIRST, tk.SEL_LAST)
+
+    @property
+    def line(self):
+        return self.index(tk.INSERT).split('.')[0]
+
+    @property
+    def column(self):
+        return self.index(tk.INSERT).split('.')[1]
+
+    @property
+    def position(self):
+        return self.index(tk.INSERT).split('.')
+
+    def scroll_to_end(self):
+        self.see(tk.END)
+        self.mark_set(tk.INSERT, tk.END)
+        self.see(tk.INSERT)
+
+    def scroll_to_start(self):
+        self.see(1.0)
+        self.mark_set(tk.INSERT, 1.0)
+        self.see(tk.INSERT)
+
+    def scroll_to_line(self, line):
+        self.see(line)
+        self.mark_set(tk.INSERT, line)
+        self.see(tk.INSERT)
+
+    # ----- Proxy (unchanged) -----
 
     def _proxy(self, *args):
         if args[0] == 'get' and (args[1] == tk.SEL_FIRST and args[2] == tk.SEL_LAST) and not self.tag_ranges(tk.SEL): 
