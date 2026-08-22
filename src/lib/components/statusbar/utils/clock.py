53fc4765a746ef4a216c568f4be258cb68895474
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-22 17:44:49 +0400

    feat: add live ticking SClock widget for the status bar

---
+++ src/lib/components/statusbar/utils/clock.py
@@ -0,0 +1,14 @@
+import time
+
+from lib.components.statusbar.utils.button import SButton
+
+class SClock(SButton):
+    def __init__(self, *args, *kwargs):
+        super().__init__(*args, **kwargs)
+        self.update()
+
+    def update(self):
+        time_live = time.strftime("%H:%M:%S")
+        self.config(text=time_live)
+        # Schedule the next update in 200ms
+        self.after(200, self.update)
