621f5dc3637d4722cd69f06ed5b09dcf80e8ac8c
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-22 18:10:03 +0400

    feat: add update_statusbar bridge method to Base class

--- src/lib/base.py
+++ src/lib/base.py
@@ -79,3 +79,15 @@ class Base:
     def open_new_window(self):
         subprocess.Popen(["python", sys.argv[0]])
         self.trace(f"Opened new window")
+
+    def update_statusbar(self):
+        if not self.active_file or self.active_file not in self.root.basepane.top.right.editortabs.opened_editors:
+            self.root.statusbar.set_line_col_info("?", "?", 0)
+            return
+
+        active_text = self.root.basepane.top.right.editortabs.get_active_tab().text
+        self.root.statusbar.set_line_col_info(
+            active_text.line,
+            active_text.column,
+            active_text.get_selected_count()
+        )
