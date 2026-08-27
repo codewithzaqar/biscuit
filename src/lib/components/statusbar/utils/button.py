f3f76c4cd63e922a6ed2c6f7f0f5ebaca7abde97
codewithzaqar <zaqarstandoff@gmail.com>
2026-08-27 15:19:32 +0400

    feat: add show/hide state management to SButton for dynamic status bar

--- src/lib/components/statusbar/utils/button.py
+++ src/lib/components/statusbar/utils/button.py
@@ -5,10 +5,22 @@ class SButton(tk.Menubutton):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
 
-        # Hardcoded styling for a dark theme
-        self.config(
-            padx=10,
-            fg="#000000",
-            activebackground="#4c4a48",
-            activeforeground="#ffffff"
-        )
+        self.enabled = False
+        self.pack_data = {}
+        self.config(padx=10, fg="#000000", activebackground="#4c4a48", activeforeground="#ffffff")
+
+    def set_pack_data(self, **kwargs):
+        self.pack_data = kwargs
+
+    def get_pack_data(self):
+        return self.pack_data
+
+    def show(self):
+        if not self.enabled:
+            self.enabled = True
+            self.pack(**self.get_pack_data())
+
+    def hide(self):
+        if self.enabled:
+            self.enabled = False
+            self.pack_forget()
