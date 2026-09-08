150e098916a2be2c0a1793b796c40e855437e9bc
codewithzaqar <zaqarstandoff@gmail.com>
2026-09-09 01:26:10 +0400

    refactor: split EditorTabs into EditorTabsPane container and tabs notebook

--- src/lib/components/placeholders/emptytab.py
+++ src/lib/components/placeholders/emptytab.py
@@ -7,7 +7,8 @@ class EmptyTab(tk.Frame):
         super().__init__(master, *args, **kwargs)
         self.base = master.base
 
-        self.logo_img = tk.PhotoImage(file=self.base.settings.resources.logo).subsample(2)
+        self.logo_img = self.base.settings.resources.logo.subsample(2)
+        
         self.logo = ttk.Label(self, image=self.logo_img, width=10)
         self.logo.pack(fill=tk.BOTH, expand=False)
 
